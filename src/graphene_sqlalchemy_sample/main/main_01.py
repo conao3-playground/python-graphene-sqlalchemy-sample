7ff9aa33084eb6fd6883a30dbcd55a53775220e8
Naoya Yamashita <conao3@gmail.com>
2023-11-27 02:44:28 +0900

    first sqlalchemy

--- src/graphene_sqlalchemy_sample/main/main_01.py
+++ src/graphene_sqlalchemy_sample/main/main_01.py
@@ -1,5 +1,46 @@
 from __future__ import annotations
 
+import sqlalchemy as sa
+import sqlalchemy.orm as sa_orm
+
+
+engine = sa.create_engine("sqlite:///:memory:", echo=True)
+session = sa_orm.Session(engine)
+
+
+@sa_orm.as_declarative()
+class Base:
+    def __repr__(self) -> str:
+        args = ", ".join(
+            [
+                f"{key}={repr(val)}"
+                for key, val in self.__dict__.items()
+                if not key.startswith("_")
+            ],
+        )
+        return f"{self.__class__.__name__}({args})"
+
+
+class User(Base):
+    __tablename__ = "user"
+
+    user_id = sa.Column(sa.String(32), primary_key=True)
+    name = sa.Column(sa.String(32), nullable=False)
+    age = sa.Column(sa.Integer, nullable=False)
+
+
+def init_db() -> None:
+    Base.metadata.create_all(engine)
+    session.add_all(
+        [
+            User(user_id="user_01", name="user_01", age=10),
+            User(user_id="user_02", name="user_02", age=20),
+            User(user_id="user_03", name="user_03", age=30),
+        ],
+    )
+    session.commit()
+
 
 def main() -> None:
-    print("main_01")
+    init_db()
+    print(session.query(User).all())
