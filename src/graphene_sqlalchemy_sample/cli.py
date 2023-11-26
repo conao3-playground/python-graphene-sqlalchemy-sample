1178ea6e519d2f86cc7f4cd4e28a411a5d366faf
Naoya Yamashita <conao3@gmail.com>
2023-11-27 02:15:54 +0900

    implement rancher

--- src/graphene_sqlalchemy_sample/cli.py
+++ src/graphene_sqlalchemy_sample/cli.py
@@ -1,2 +1,23 @@
+import argparse
+import importlib
+import pathlib
+
+
+def get_mains() -> list[str]:
+    return [
+        elm.stem[len('main_'):]
+        for elm in pathlib.Path(__file__).parent.glob('main/*.py')
+        if elm.stem != '__init__'
+    ]
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--main', choices=get_mains(), required=True)
+    return parser.parse_args()
+
+
 def main() -> None:
-    print('hello')
+    args = parse_args()
+    module = importlib.import_module(f'.main.main_{args.main}', package=__package__)
+    module.main()
