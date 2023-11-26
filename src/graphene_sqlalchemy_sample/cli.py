022dda67c2a1d7a7f18ba0a8c2bc7a72868fcd50
Naoya Yamashita <conao3@gmail.com>
2023-11-27 02:18:55 +0900

    fmt

--- src/graphene_sqlalchemy_sample/cli.py
+++ src/graphene_sqlalchemy_sample/cli.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import argparse
 import importlib
 import pathlib
@@ -5,19 +7,19 @@ import pathlib
 
 def get_mains() -> list[str]:
     return [
-        elm.stem[len('main_'):]
-        for elm in pathlib.Path(__file__).parent.glob('main/*.py')
-        if elm.stem != '__init__'
+        elm.stem[len("main_") :]
+        for elm in pathlib.Path(__file__).parent.glob("main/*.py")
+        if elm.stem != "__init__"
     ]
 
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
-    parser.add_argument('--main', choices=get_mains(), required=True)
+    parser.add_argument("--main", choices=get_mains(), required=True)
     return parser.parse_args()
 
 
 def main() -> None:
     args = parse_args()
-    module = importlib.import_module(f'.main.main_{args.main}', package=__package__)
+    module = importlib.import_module(f".main.main_{args.main}", package=__package__)
     module.main()
--- src/graphene_sqlalchemy_sample/main/main_01.py
+++ src/graphene_sqlalchemy_sample/main/main_01.py
@@ -1,2 +1,5 @@
+from __future__ import annotations
+
+
 def main() -> None:
-    print('main_01')
+    print("main_01")
